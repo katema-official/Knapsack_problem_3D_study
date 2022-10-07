03a9e45a102ac39603a5bb48ca32e3f54cea8a80
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-10-07 14:39:21 +0200

    Addead feasibility test in python code

--- Knapsack_3d_study_0/main4.py
+++ Knapsack_3d_study_0/main4.py
@@ -3,8 +3,48 @@ import math
 
 import ursina
 from boxmodule_ursina import *
+from boxmodule_misc import *
 
-file = open("C:\\Users\\aless\\Desktop\\Knapsack_problem_3D_study\\C_stuff\\final_results.txt", "r")
+file = open("C:\\Users\\aless\\Desktop\\Knapsack_problem_3D_study\\C_stuff\\Interesting_data\\final_results_strict.txt", "r")
+
+def isOverlapping1D(box1,box2):
+    return box1[1] > box2[0] and box2[1] > box1[0]
+
+def check_if_boxes_overlap_2(boxes):
+    overlap = False
+    for i in range(len(boxes)):
+        boxes_copy = boxes[i + 1:]
+        for j in range(len(boxes_copy)):
+            x0_a = boxes[i].x0
+            x0_b = boxes_copy[j].x0
+            xlen_a = x0_a + boxes[i].xlen
+            xlen_b = x0_b + boxes_copy[j].xlen
+
+            box1x = (x0_a, xlen_a)
+            box2x = (x0_b, xlen_b)
+
+            y0_a = boxes[i].y0
+            y0_b = boxes_copy[j].y0
+            ylen_a = y0_a + boxes[i].ylen
+            ylen_b = y0_b + boxes_copy[j].ylen
+
+            box1y = (y0_a, ylen_a)
+            box2y = (y0_b, ylen_b)
+
+            z0_a = boxes[i].z0
+            z0_b = boxes_copy[j].z0
+            zlen_a = z0_a + boxes[i].zlen
+            zlen_b = z0_b + boxes_copy[j].zlen
+
+            box1z = (z0_a, zlen_a)
+            box2z = (z0_b, zlen_b)
+
+            if isOverlapping1D(box1x, box2x) and isOverlapping1D(box1y, box2y) and isOverlapping1D(box1z, box2z):
+                print(str(i) + " e " + str(i + j) + " si sovrappongono")
+                overlap = True
+    return overlap
+
+
 
 class MyBoxes:
     boxes_new = []
@@ -33,8 +73,11 @@ def make_a_step():
     b = [int(elem) for elem in file.readline()[:-1].split(" ")[:-1]]
     c = [int(elem) for elem in file.readline()[:-1].split(" ")[:-1]]
 
-    place_boxes_in_space(MyBoxes.boxes_new)
+    place_boxes_in_space(MyBoxes.boxes_new, False)
     # print("best_volume = " + str(SimulatedAnnealing.best_volume / (cont_x * cont_y * cont_z)))
+
+
+
 
 def input(key):
     if key == "l":
@@ -44,9 +87,19 @@ def input(key):
             make_a_step()
 
 
-def place_boxes_in_space(boxes):
+def place_boxes_in_space(boxes, all=False):
+    boxes_effectively_placed = []
+
     for box in boxes:
-        box.entity = place_box(box.x0, box.y0, box.z0, box.xlen, box.ylen, box.zlen, box.color)
+        if all == True:
+            box.entity = place_box(box.x0, box.y0, box.z0, box.xlen, box.ylen, box.zlen, box.color)
+            boxes_effectively_placed.append(box)
+        else:
+            if(box.x0 + box.xlen <= cont_x and box.y0 + box.ylen <= cont_y and box.z0 + box.zlen <= cont_z):
+                box.entity = place_box(box.x0, box.y0, box.z0, box.xlen, box.ylen, box.zlen, box.color)
+                boxes_effectively_placed.append(box)
+
+    if check_if_boxes_overlap_2(boxes_effectively_placed): print("Si sovrappongono")
 
 def destroy_placed_boxes_in_space(boxes):
     for box in boxes:
